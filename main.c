fdf876a2c10c439b986be1402b3951392944f00c
yenwei100606 <yenwei100606@gmail.com>
2019-03-20 01:53:26 +0800

    HW03

--- main.c
+++ main.c
@@ -1,5 +1,19 @@
+int addten(int a)
+{
+	int b=a+10;
+	return b;
+}
+
+int test5parameter(int a,int b,int c,int d,int e)
+{
+	return (a*1)+(b*2)+(c*3)+(d*4)+(e*5);
+}
+
+
 void reset_handler(void)
 {
+	int x=addten(5);
+	int y=test5parameter(1,2,3,4,5);
 	while (1)
 		;
 }
